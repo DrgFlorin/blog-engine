ec5d4661ecbfc1a2ba093f660327b472861eb3f0
DrgFlorin <drgflorin.dev@gmail.com>
2018-12-26 01:21:55 +0200

    homepage and post backend

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
+<h2><a href="administrator/">New Post</a></h2>
+
 <?php
 include 'configs/_db.php';
 
-$query = "SELECT title, date, content, allow_comments, author, img_url FROM clity_posts";
+$query = "SELECT id, title, date, content, allow_comments, author, img_url FROM clity_posts ORDER BY id DESC";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "title: ".$row['title']."<br> date: ".$row['date']."<br> content: ".$row['content']."<br> allow_comments: ".$row['allow_comments']."<br> author".$row['author']."<br> img link: <img src=\"".$row['img_url']."\"><br><br>";
+        echo "title: <a href=\"post?s=".$row['id']."\">".$row['title']."</a><br> date: ".$row['date']."<br> content: ".substr($row['content'], 0,50)."...<br> allow_comments: ".$row['allow_comments']."<br> author: ".$row['author']."<br> img link: <img src=\"".$row['img_url']."\"><br><br>";
     }
 }
 ?>
--- post/index.php
+++ post/index.php
@@ -1,6 +1,23 @@
 <?php
-session_start();
-$title = $_SESSION['title'];
+include '../configs/_db.php';
+$id = $_REQUEST['s'];
+
+$query = "SELECT title, date, content, allow_comments, author, img_url FROM clity_posts WHERE id='$id'";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $title = $row['title'];
+        $date = $row['date'];
+        $content = $row['content'];
+        $allow_comments = $row['allow_comments'];
+        $author = $row['author'];
+        $img_url = $row['img_url'];
+    }
+} else {
+    $title = "NO POSTS FOUND / SO NO TITLES FOUND";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +29,21 @@ $title = $_SESSION['title'];
     <title><?php echo $title; ?></title>
 </head>
 <body>
-    
+<h2><a href="../">Go back</a></h2>
+<?php
+echo "<h2>$title</h2>\n";
+echo "Author: ".$author."<br>";
+echo $date."<br>";
+echo "<img src=\"".$img_url."\"><br>";
+echo $content."<br>";
+if ($allow_comments == true) {
+    echo "<br>Comments are allowed<br>";
+} else {
+    echo "<br>Comments are disabled<br>";
+}
+
+
+?>
+
 </body>
 </html>
