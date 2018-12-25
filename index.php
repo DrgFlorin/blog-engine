b181d21106ed5b43879439dc5e129b6c179557bf
DrgFlorin <bayat.bun@gmail.com>
2018-12-25 21:22:07 +0200

    post connect

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+error_reporting(0);
+include 'configs/_db.php';
+$title = $_POST['title'];
+$content = $_POST['content'];
+$allow_comments = $_POST['allow_comments'];
+$author = $_POST['author'];
+$img_url = $_POST['img_url'];
+
+$query = "INSERT INTO clity_posts (`title`, `content`, `allow_comments`, `author`, `img_url`) VALUES ('$title', '$content', '$allow_comments', '$author', '$img_url')";
+
+if (isset($_POST['addBtn'])) {
+    if ($conn->query($query) === true) {
+        echo "Post Added";
+    } else {
+        echo "Error: " .$query . "<br>". $conn->error;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +26,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Clity - Blog engine</title>
 </head>
+<style>
+    input, select, .question {
+        display:block;
+    }
+    input {
+        margin: 5px 0;
+    }
+</style>
 <body>
-    <form action="index.php" method="GET">
-        <input type="text">
+    <form action="index.php" method="POST">
+        <input type="text" name="title" placeholder="title...">
+
+        <textarea rows="4" cols="50" name="content" placeholder="content..."></textarea>
+        <div class="question">Allow Comments?</div>
+        <select name="allow_comments">
+            <option value="1">Yes</option>
+            <option value="0">No</option>
+        </select>
+
+        <input type="number" name="author" placeholder="author...ID">
+
+        <input type="text" name="img_url" placeholder="image...">
+
+        <input type="submit" name="addBtn" value="Add Post">
     </form>
 </body>
 </html>
